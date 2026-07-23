19de88bb1508c6dd90c178f3d3947c80fb60e243
Karan Sardar <snsardarkaran61@gmail.com>
2026-07-23 19:47:31 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict";
+//alert(3+3);//using node js not browser
+//null = standalone value that represents nothingness
+//undefined = value that represents a variable that has not been assigned a value yet
+//symbol = a unique identifier that can be used as a key for object properties
+
+console.log(typeof "hello"); //string
+console.log(typeof null); //object because of a bug in JavaScript, null is considered an object type
